9283f0496cc6618dfd380c49707aee897f20b6de
ShriyanYamali <shriyanyamali09@gmail.com>
2026-04-26 13:49:29 -0400

    remove candidate committee IEs

--- scripts/fec_data_loader.py
+++ scripts/fec_data_loader.py
@@ -325,6 +325,23 @@ class FECDataLoader:
                 self.ie_data_presidential = merged
         
         print("Committee information merged for all datasets")
+
+        for dataset_name, dataset in [
+            ('ie_data_filtered', self.ie_data_filtered),
+            ('ie_data_senate', self.ie_data_senate),
+            ('ie_data_presidential', self.ie_data_presidential)
+        ]:
+            if len(dataset) == 0:
+                continue
+
+            cleaned = dataset[~dataset['CMTE_TP'].isin(['H', 'S', 'P'])].copy()
+
+            if dataset_name == 'ie_data_filtered':
+                self.ie_data_filtered = cleaned
+            elif dataset_name == 'ie_data_senate':
+                self.ie_data_senate = cleaned
+            elif dataset_name == 'ie_data_presidential':
+                self.ie_data_presidential = cleaned
         
         print("\nChecking for Super PAC activity in pre-2011 cycles...")
         for dataset_name, dataset in [
